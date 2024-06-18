93c7d2215efe8043cba6486c6e148ce361195750
leoxanigm <zola@zola.dev>
2024-06-18 20:07:15 +0200

    add shadcnUI

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className='flex justify-center p-4'>
+      <Button size="default" variant="outline">Click me</Button>
+    </div>
+  );
+}
